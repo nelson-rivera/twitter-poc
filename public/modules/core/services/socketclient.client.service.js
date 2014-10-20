3e97398667b066c6e300bfd717c3052f8b3ec1e9
Nelson Rivera <nelson@nelson-laptop>
2014-10-19 19:29:38 -0600

    Structure and modules

---
+++ public/modules/core/services/socketclient.client.service.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('core').factory('Socketclient', [
+	function() {
+		// Socketclient service logic
+		// ...
+
+		// Public API
+		return {
+			someMethod: function() {
+				return true;
+			}
+		};
+	}
+]);
